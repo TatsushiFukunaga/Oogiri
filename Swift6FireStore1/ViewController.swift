33d32e0d82d2bb4379dacfe17bc4d5f207f01771
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-07 12:45:32 +0900

    Initial Commit

---
+++ Swift6FireStore1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6FireStore1
+//
+//  Created by Tatsushi Fukunaga on 2021/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
