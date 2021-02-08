9a502469622ef9f24821b512031d4ad4e00c37ce
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-08 16:16:19 +0900

    delegate

--- Swift6FireStore1/Controller/CheckViewController.swift
+++ Swift6FireStore1/Controller/CheckViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
-class CheckViewController: UIViewController {
-
+class CheckViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     var odaiString = String()
     
     @IBOutlet weak var odaiLabel: UILabel!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    let db = Firestore.firestore()
+    var dataSets : [AnswersModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +26,32 @@ class CheckViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         odaiLabel.text = odaiString
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        loadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func loadData() {
+        // get documents of Answers
+        
+        
+        // into DataSets as AnswersModel
+    }
+    
+
 
     /*
     // MARK: - Navigation
